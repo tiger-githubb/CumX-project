73d8ee7b3092c7837c5228ac985be9028f8956c5
tiger-githubb <aristidetiger12@gmail.com>
2023-07-06 21:22:31 +0000

    Migrations

---
+++ database/migrations/2023_07_06_205939_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nom');
+            $table->string('numero_telephone');
+            $table->integer('nombre_produit');
+            $table->string('localisation');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
