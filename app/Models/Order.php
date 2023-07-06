8037a60e45f22c23aab5ba74d9a4c1902cd0aef8
tiger-githubb <aristidetiger12@gmail.com>
2023-07-06 21:28:50 +0000

    models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'nom', 'numero_telephone', 'nombre_produit', 'localisation'
+    ];
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'order_product')
+            ->withPivot('quantity')
+            ->withTimestamps();
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'nom', 'description', 'stock', 'prix', 'image'
+    ];
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'order_product')
+            ->withPivot('quantity')
+            ->withTimestamps();
+    }
 }
